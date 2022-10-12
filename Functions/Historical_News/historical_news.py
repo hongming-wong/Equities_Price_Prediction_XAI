94ceba954a730a88669c0ce763408a340c0de540
SohWaiHeng <sohwaiheng1023@gmail.com>
2022-10-12 18:27:12 +0800

    news and sentiment API added

--- Functions/Historical_News/historical_news.py
+++ Functions/Historical_News/historical_news.py
@@ -7,15 +7,29 @@ client = EodHistoricalData(API_KEY)
 
 tickers = sp500.get_sp500_tickers()
 
-def get_news(ticker, start, end):
-
+def get_sentiment(ticker, start, end):
+    """
+    Get sentiment score of news computed by eod for a specific stock
+    """
     try:
         data = client.get_sentiment(s=ticker, from_=start, to=end)
-        df = pd.DataFrame(data)
-        return df
+        return data
+
+    except Exception as ex:
+        print("Error getting the sentiment of the news")
+        print(ex)
+
+def get_news_by_ticker(ticker, start, end, limit=50):
+    """
+    Get news data for a specific stock
+    """
+    try:
+        data = client.get_financial_tweets(s=ticker, from_=start, to=end, limit=limit)
+        return data
 
     except Exception as ex:
         print("Error getting the news")
         print(ex)
 
-print(get_news('AAPL.US', '2020-01-05', '2020-10-10'))
+
+print(get_news_by_ticker('AAPL.US', '2020-01-05', '2020-10-10'))
