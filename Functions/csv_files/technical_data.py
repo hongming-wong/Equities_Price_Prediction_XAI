fcb0bf08c502f45f6074709abcf0c311403a5163
Hong Ming <hongming2000@gmail.com>
2022-10-20 00:15:19 +0800

    Tested basic LSTM model

--- Functions/csv_files/technical_data.py
+++ Functions/csv_files/technical_data.py
@@ -1,23 +1,28 @@
 from Functions.Technical_Indicators import api_technical
 from Functions.sp500 import sp500
+import os
 
-start = '2000-01-01'
+start = '1995-01-01'
 end = '2021-12-31'
 
 train, dev, test = sp500.get_sp500_tickers(split=True)
-dev = dev[:4]
 
-periods = [30, 50, 100]
+train = train[:200]
+periods = [100]
 ta = ['sma', 'ema', 'wma', 'volatility', 'stochastic', 'slope']
 
 
 def download(tickers, period, prefix=""):
+    path = f"technical_data/{prefix}_period_{period}.csv"
     for ticker in tickers:
-        api_technical.get_technical_data(ticker, ta, start, end, period=period).\
-            to_csv(f"technical_data/{prefix}_period_{period}.csv")
+        print("Downloading", ticker)
+        data = api_technical.get_technical_data(ticker, ta, start, end, period=period)
+        data['ticker'] = ticker
+        data.to_csv(path, mode='a', header=not os.path.exists(path), index=False)
 
 
 for p in periods:
+    print(f"Downloading for period {p}")
     download(dev, period=p, prefix="dev")
     print("Finish Downloading Dev")
     download(test, period=p, prefix="test")
--- readme.md
+++ readme.md
@@ -10,3 +10,9 @@ Soh Wai Heng 3035709502
 2. Change to virtual environment by running the command "source venv/bin/activate"
 3. Install required packages in the virtual environment by running the command "pip install -r requirements.txt"
 4. Within Functions/, add a config file with a variable called API_KEY.
+
+
+## Data Sources:
+
+The data files for Functions/csv_files can be downloaded from 
+[here](https://connecthkuhk-my.sharepoint.com/:f:/g/personal/u3571968_connect_hku_hk/Ens31blhpIBOhIYzW6-WoXEBgOP8fItqsAfL-xxh9QffzQ?e=6RuGhE).
