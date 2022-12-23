9fd5902adeda1a8344a974efc33974051fca4976
SohWaiHeng <sohwaiheng1023@gmail.com>
2022-12-23 09:28:36 +0800

    bert update

--- Functions/Historical_News/historical_news.py
+++ Functions/Historical_News/historical_news.py
@@ -8,8 +8,6 @@ from datetime import date, timedelta
 
 client = EodHistoricalData(API_KEY)
 
-tickers = sp500.get_sp500_tickers()
-
 def get_news_by_ticker(ticker, start, end, offset=0, limit=1000):
     """
     Get news data for a specific stock in the format of dataframe (columns = date, content)
@@ -30,7 +28,7 @@ def get_news_to_price_changes(ticker, start, end, offset=0, limit=1000, period=5
     Get news to price change for a specific stock in the format of dataframe (columns = date, content, price_change)
     """
     try:
-        news = get_all_news(ticker, start, end, offset, limit)
+        news = get_all_news_by_ticker(ticker, start, end, offset, limit)
         price = historical_prices.get_daily_price_changes(ticker, start, end, period)
 
         for t, p in price.items():
@@ -54,7 +52,7 @@ def daterange(start_date, end_date):
         print("Error getting the news to price changes")
         print(ex)
 
-def get_all_news(ticker, start, end, offset=0, limit=1000):
+def get_all_news_by_ticker(ticker, start, end, offset=0, limit=1000):
     """
     Get all the news from start to end of a specific stock
     """
@@ -68,8 +66,33 @@ def get_all_news(ticker, start, end, offset=0, limit=1000):
         return df
 
     except Exception as ex:
-        print("Error getting the news to price changes")
+        print("Error getting all news for a single stock")
         print(ex)
 
-print(get_all_news('MSFT.US', date(2021, 6, 1), date(2021, 9, 30)))
+def get_all_news(start, end, offset=0, limit=1000):
+    """
+    Get all the news from start to end of all stocks separated into train and test sets
+    """
+    try:
+        tickers = sp500.get_sp500_tickers()
+        train, test = tickers[:450], tickers[450:]
+        train_df, test_df = pd.DataFrame(columns=['date', 'content']), pd.DataFrame(columns=['date', 'content'])
+        for t in train:
+            print(f'extracting news for {t} ...')
+            new_df = get_news_to_price_changes(t, start, end, offset, limit)
+            dup_df = train_df.append(new_df, ignore_index=True)
+            train_df = dup_df
+        for t in test:
+            print(f'extracting news for {t} ...')
+            new_df = get_news_to_price_changes(t, start, end, offset, limit)
+            dup_df = test_df.append(new_df, ignore_index=True)
+            test_df = dup_df
+        
+        return train_df, test_df
+
+    except Exception as ex:
+        print("Error getting all the news")
+        print(ex)
+
+# print(get_all_news('MSFT.US', date(2021, 6, 1), date(2021, 9, 30)))
 # print(get_news_by_ticker('MSFT.US', '2021-11-10', '2021-11-10'))
