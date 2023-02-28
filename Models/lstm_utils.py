cb0c8559b550c8f21e2a8334879436f9ef940a8e
Hong Ming <52535157+hongming-wong@users.noreply.github.com>
2023-02-28 20:39:11 +0800

    Turning to MSE model

--- Models/lstm_utils.py
+++ Models/lstm_utils.py
@@ -13,21 +13,21 @@ def plot_history(history, title):
     ax1.set_xlabel('Epoch')
     ax1.set_ylabel('Accuracy')
     ax2.set_ylabel('Loss')
-
-    ax1.plot(history.history['acc'], color = 'g')
-    ax2.plot(history.history['loss'], color = 'r')
+    if "acc" in history.history.keys():
+        ax1.plot(history.history['acc'], color='g')
+    ax2.plot(history.history['loss'], color='r')
     ax1.set_title(title)
 
-
     if "val_loss" in history.history.keys():
-        print(history.history.keys())
-        ax1.plot(history.history['val_acc'], color = 'b')
-        legend.append('val_acc')
-        ax2.plot(history.history['val_loss'], color = 'pink')
+        ax2.plot(history.history['val_loss'], color='pink')
         legend_2.append('val_loss')
 
+    if "val_acc" in history.history.keys():
+        ax1.plot(history.history['val_acc'], color='b')
+        legend.append('val_acc')
+
     ax1.legend(legend, loc='upper left')
-    ax2.legend(legend_2, loc = 'upper right')
+    ax2.legend(legend_2, loc='upper right')
 
     plt.show()
 
@@ -40,11 +40,15 @@ def scale_column(x, column=0):
     mms = MinMaxScaler()
     x1 = x.copy()
     # The volume column has index 0
-    x1[:, column] = mms.fit_transform(x1[:, column].reshape(timesteps, 1)).reshape(timesteps, )
+    x1[:, column] = mms.fit_transform(
+        x1[:, column].reshape(timesteps, 1)).reshape(timesteps, )
     return x1
 
 
-check_rsi = lambda rsi: 0 if rsi is np.nan or (70 > rsi > 30) else (-1 if rsi < 30 else 1)
+def check_rsi(rsi): return 0 if rsi is np.nan or (
+    70 > rsi > 30) else (-1 if rsi < 30 else 1)
+
+
 """
 Traditionally, RSI > 70 = Overbought, RSI < 30 Oversold
 We transform the column to -1, 0, 1,
@@ -58,6 +62,7 @@ def rsi_transformer(x, column):
     x2[:, 4] = vectorized_check_rsi(rsi_column)
     return x2
 
+
 def check_dmi(x):
     """https://capital.com/average-directional-index
     According to this,
@@ -70,17 +75,20 @@ def check_dmi(x):
 
     if x is np.nan:
         return 0
-    elif x > 50: #Very Strong Trend
+    elif x > 50:  # Very Strong Trend
         return 3
-    elif x > 40: #Strong Trend
+    elif x > 40:  # Strong Trend
         return 2
-    elif x > 20: # Trend
+    elif x > 20:  # Trend
         return 1
-    else: # No Trend
+    else:  # No Trend
         return 0
+
+
 vectorized_check_dmi = np.vectorize(check_dmi)
 
-def dmi_transformer(x, column = 9):
+
+def dmi_transformer(x, column=9):
     x2 = x.copy()
     dmi_26_column = x2[:, column]
     x2[:, column] = vectorized_check_dmi(dmi_26_column)
